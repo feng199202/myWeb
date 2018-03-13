5875d5430b91e84f80ac3874cf751e8b3198a175
jibin <jibin@ifeng.com>
2018-03-13 09:51:28 +0800

    hello world

---
+++ springWeb/src/main/java/com/my/myweb/security/MyPasswordEncoder.java
@@ -0,0 +1,20 @@
+package com.my.myweb.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by jibin on 2018/3/13.
+ */
+public class MyPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence arg0) {
+        return arg0.toString();
+    }
+
+    @Override
+    public boolean matches(CharSequence arg0, String arg1) {
+        return arg1.equals(arg0.toString());
+    }
+
+}
